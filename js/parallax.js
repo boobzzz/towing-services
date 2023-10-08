95a0319e4b29ebba4104e8d16f269e469bc66026
Yuriy Borys <iam618384@gmail.com>
2023-10-08 17:59:01 +0300

    Bugs tests on ios

--- js/parallax.js
+++ js/parallax.js
@@ -10,43 +10,75 @@ const halfAnimationDuration = animationDuration / 2;
 export function initParallax() {
     truck.addEventListener('animationend', onTruckAnimationEnd);
     truck.addEventListener('webkitAnimationEnd', onTruckAnimationEnd);
-    header1.addEventListener('animationend', playSubHeadersAppearAnimation);
-    header1.addEventListener('webkitAnimationEnd', playSubHeadersAppearAnimation);
-    header3.addEventListener('animationend', playCallButtonAppearAnimation);
-    header3.addEventListener('webkitAnimationEnd', playCallButtonAppearAnimation);
+    header1.addEventListener('animationend', onMainHeaderAppearAnimationEnd);
+    header1.addEventListener('webkitAnimationEnd', onMainHeaderAppearAnimationEnd);
+    header3.addEventListener('animationend', onSubHeadersAppearAnimationEnd);
+    header3.addEventListener('webkitAnimationEnd', onSubHeadersAppearAnimationEnd);
+    roadSign.addEventListener('animationend', onCallButtonAppearAnimationEnd);
+    roadSign.addEventListener('webkitAnimationEnd', onCallButtonAppearAnimationEnd);
     roadSign.addEventListener("pointerup", playCallButtonOnTapAnimation);
     playTruckAppearAnimation();
 }
 
 function playTruckAppearAnimation() {
-    truck.style.animation = getAnimationParams('truckSlideInAnimation', animationDuration);
+    if (truck.style.display !== 'none') {
+        // truck.style.display = 'block';
+        truck.style.animation = getAnimationParams('truckSlideInAnimation', animationDuration);
+    }
 }
 
 function onTruckAnimationEnd() {
+    truck.style.animation = '';
     playMainHeaderAppearAnimation();
     playPhoneAppearAnimation();
 }
 
 function playMainHeaderAppearAnimation() {
-    header1.style.visibility = "visible";
+    // header1.style.visibility = "visible";
+    // header1.style.display = 'block';
     header1.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
 }
 
+function playPhoneAppearAnimation() {
+    // phone.style.visibility = "visible";
+    // phone.style.display = 'block';
+    phone.style.animation = getAnimationParams('phoneSlideInAnimation', halfAnimationDuration);
+}
+
+function onMainHeaderAppearAnimationEnd() {
+    header1.style.opacity = 1;
+    phone.style.opacity = 1;
+    header1.style.animation = '';
+    phone.style.animation = '';
+    playSubHeadersAppearAnimation();
+}
+
 function playSubHeadersAppearAnimation() {
-    header2.style.visibility = "visible";
-    header3.style.visibility = "visible";
+    // header2.style.visibility = "visible";
+    // header3.style.visibility = "visible";
+    // header2.style.display = 'block';
+    // header3.style.display = 'block';
     header2.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
     header3.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
 }
 
+function onSubHeadersAppearAnimationEnd() {
+    header2.style.opacity = 1;
+    header3.style.opacity = 1;
+    header2.style.animation = '';
+    header3.style.animation = '';
+    playCallButtonAppearAnimation();
+}
+
 function playCallButtonAppearAnimation() {
-    roadSign.style.visibility = "visible";
+    // roadSign.style.visibility = "visible";
+    // roadSign.style.display = 'flex';
     roadSign.style.animation = getAnimationParams('callButtonAppearAnimation', animationDuration);
 }
 
-function playPhoneAppearAnimation() {
-    phone.style.visibility = "visible";
-    phone.style.animation = getAnimationParams('phoneSlideInAnimation', halfAnimationDuration);
+function onCallButtonAppearAnimationEnd() {
+    roadSign.style.opacity = 1;
+    roadSign.style.animation = '';
 }
 
 function playCallButtonOnTapAnimation() {
