a55af4b4639dc9d28676b2800afd8b61f01128a7
Yuriy Borys <iam618384@gmail.com>
2023-10-09 20:16:03 +0300

    Bugs tests on ios

--- js/parallax.js
+++ js/parallax.js
@@ -8,17 +8,17 @@ const animationDuration = 0.8;
 const halfAnimationDuration = animationDuration / 2;
 
 export function initParallax() {
-    truck.addEventListener('animationend', onTruckAnimationEnd);
-    truck.addEventListener('webkitAnimationEnd', onTruckAnimationEnd);
-    header1.addEventListener('animationend', onMainHeaderAppearAnimationEnd);
-    header1.addEventListener('webkitAnimationEnd', onMainHeaderAppearAnimationEnd);
-    header3.addEventListener('animationend', onSubHeadersAppearAnimationEnd);
-    header3.addEventListener('webkitAnimationEnd', onSubHeadersAppearAnimationEnd);
-    roadSign.addEventListener('animationend', onCallButtonAppearAnimationEnd);
-    roadSign.addEventListener('webkitAnimationEnd', onCallButtonAppearAnimationEnd);
+    truck.addEventListener("animationend", onTruckAnimationEnd);
+    truck.addEventListener("webkitAnimationEnd", onTruckAnimationEnd);
+    header1.addEventListener("animationend", onMainHeaderAppearAnimationEnd);
+    header1.addEventListener("webkitAnimationEnd", onMainHeaderAppearAnimationEnd);
+    header3.addEventListener("animationend", onSubHeadersAppearAnimationEnd);
+    header3.addEventListener("webkitAnimationEnd", onSubHeadersAppearAnimationEnd);
+    roadSign.addEventListener("animationend", onCallButtonAppearAnimationEnd);
+    roadSign.addEventListener("webkitAnimationEnd", onCallButtonAppearAnimationEnd);
     roadSign.addEventListener("pointerup", playCallButtonOnTapAnimation);
 
-    if (window.getComputedStyle(truck.parentElement).display === 'none') {
+    if (window.getComputedStyle(truck.parentElement).display === "none") {
         playMainHeaderAppearAnimation();
         playPhoneAppearAnimation();
     } else {
@@ -27,56 +27,56 @@ export function initParallax() {
 }
 
 function playTruckAppearAnimation() {
-    truck.style.animation = getAnimationParams('truckSlideInAnimation', animationDuration);
+    truck.style.animation = getAnimationParams("truckSlideInAnimation", animationDuration);
 }
 
 function onTruckAnimationEnd() {
-    truck.style.opacity = 1;
-    truck.style.animation = '';
+    truck.style.opacity = "1";
+    truck.style.animation = "";
     playMainHeaderAppearAnimation();
     playPhoneAppearAnimation();
 }
 
 function playMainHeaderAppearAnimation() {
-    header1.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
+    header1.style.animation = getAnimationParams("infoHeadersSlideInAnimation", halfAnimationDuration);
 }
 
 function playPhoneAppearAnimation() {
-    phone.style.animation = getAnimationParams('phoneSlideInAnimation', halfAnimationDuration);
+    phone.style.animation = getAnimationParams("phoneSlideInAnimation", halfAnimationDuration);
 }
 
 function onMainHeaderAppearAnimationEnd() {
-    header1.style.opacity = 1;
-    phone.style.opacity = 1;
-    header1.style.animation = '';
-    phone.style.animation = '';
+    header1.style.opacity = "1";
+    phone.style.opacity = "1";
+    header1.style.animation = "";
+    phone.style.animation = "";
     playSubHeadersAppearAnimation();
 }
 
 function playSubHeadersAppearAnimation() {
-    header2.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
-    header3.style.animation = getAnimationParams('infoHeadersSlideInAnimation', halfAnimationDuration);
+    header2.style.animation = getAnimationParams("infoHeadersSlideInAnimation", halfAnimationDuration);
+    header3.style.animation = getAnimationParams("infoHeadersSlideInAnimation", halfAnimationDuration);
 }
 
 function onSubHeadersAppearAnimationEnd() {
-    header2.style.opacity = 1;
-    header3.style.opacity = 1;
-    header2.style.animation = '';
-    header3.style.animation = '';
+    header2.style.opacity = "1";
+    header3.style.opacity = "1";
+    header2.style.animation = "";
+    header3.style.animation = "";
     playCallButtonAppearAnimation();
 }
 
 function playCallButtonAppearAnimation() {
-    roadSign.style.animation = getAnimationParams('callButtonAppearAnimation', animationDuration);
+    roadSign.style.animation = getAnimationParams("callButtonAppearAnimation", animationDuration);
 }
 
 function onCallButtonAppearAnimationEnd() {
-    roadSign.style.opacity = 1;
-    roadSign.style.animation = '';
+    roadSign.style.opacity = "1";
+    roadSign.style.animation = "";
 }
 
 function playCallButtonOnTapAnimation() {
-    roadSign.style.animation = getAnimationParams('callButtonOnTapAnimation', animationDuration / 4);
+    roadSign.style.animation = getAnimationParams("callButtonOnTapAnimation", animationDuration / 4);
 }
 
 function getAnimationParams(animation, duration) {
